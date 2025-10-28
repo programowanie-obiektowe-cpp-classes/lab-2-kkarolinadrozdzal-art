7a1e2abc218ca29e09970560fa119aa9cee3414c
Karolina Drożdżal <k.karolinadrozdzal@gmail.com>
2025-10-28 21:36:27 +0100

    Update ResourceManager.hpp

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,43 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+    Resource * bernard;
+ResourceManager() {
+    bernard = new Resource();
+}
+ResourceManager(const ResourceManager& other) {
+    bernard = new Resource(*other.bernard);
+}
+
+ResourceManager(ResourceManager&& other) noexcept {
+
+    bernard = other.bernard; 
+}
+
+ResourceManager& operator=(const ResourceManager& other)
+{
+    if (this != &other)
+    {
+        delete bernard;
+        bernard = new Resource(*other.bernard);
+    }
+    return *this;
+}
+ResourceManager& operator=(ResourceManager& other) noexcept
+{
+    if (this != &other)
+    {
+        delete bernard;
+        bernard = other.bernard;
+    }
+    return *this;
+}
+
+double get() {
+    return bernard->get();
+}
+
+~ResourceManager() {
+    delete bernard;
+}
 };
