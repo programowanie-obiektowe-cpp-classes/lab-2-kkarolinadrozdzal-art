11a40f7586ffd9f1b1439db674dad60aac7b23fe
Karolina Drożdżal <k.karolinadrozdzal@gmail.com>
2025-10-28 22:05:24 +0100

    Update ResourceManager.hpp
    :(

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,50 +4,54 @@
 
 class ResourceManager
 { 
-public:
     Resource * bernard;
-ResourceManager() {
-    bernard = new Resource();
-}
-ResourceManager(const ResourceManager& other) {
+public:
+    ResourceManager() {
+        bernard = new Resource();
+    }
+    ResourceManager(const ResourceManager& other) 
+    {
         if (other.bernard != nullptr) {
             bernard = new Resource(*other.bernard);
         }
-}
-
-ResourceManager(ResourceManager&& other) noexcept {
-
-    bernard = other.bernard; 
-}
-
-ResourceManager& operator=(const ResourceManager& other)
-{
-    if (this != &other)
- {
-     if (bernard != nullptr)
-     {
-         delete bernard;
-     }
-         bernard = new Resource(*other.bernard);
-     
- }
- return *this;
-}
-ResourceManager& operator=(ResourceManager& other) noexcept
-{
-    if (this != &other)
-    {
-        delete bernard;
-        bernard = other.bernard;
+        bernard = nullptr;
     }
-    return *this;
-}
+    
+    ResourceManager(ResourceManager&& other) noexcept {
 
-double get() {
-    return bernard->get();
-}
+        bernard = other.bernard; 
+        other.bernard = nullptr;
+    }
 
-~ResourceManager() {
-    delete bernard;
-}
+    ResourceManager& operator=(const ResourceManager& other)
+    {
+        if (this != &other)
+        {
+            if (bernard != nullptr)
+            {
+                delete bernard;
+            }
+                bernard = new Resource(*other.bernard);
+            
+        }
+        return *this;
+    }
+    ResourceManager& operator=(ResourceManager&& other) noexcept
+    {
+        if (this != &other)
+        {
+            delete bernard;
+            bernard = other.bernard;
+            other.bernard = nullptr;
+        }
+        return *this;
+    }
+
+    double get() {
+        return bernard->get();
+    }
+
+    ~ResourceManager() {
+        delete bernard;
+    }
 };
